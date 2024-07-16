f7d481f564f344f1ac6bb4eadf3809d2ea1dffb7
ysdayone <166668156+ysdayone@users.noreply.github.com>
2024-07-17 03:40:05 +0900

    kadai_007アップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCUTYPE html>
+<html lang="ja">
+  
+<head>
+  <meta charset="utf-8">
+  <title>PHP基礎編_第7章課題</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 連想配列を作成する
+    $Array = ['name' => 'onion', 'price' => 200, 'wight' => 160];
+
+    // 値を出力する
+    print_r($Array);
+
+    ?>
+  </p>
+</body>
+
+</html>
